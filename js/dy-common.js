f16070657b4efa00aeca3164068b102cad1d4290
18826286257 <32836549+18826286257@users.noreply.github.com>
2020-03-15 18:29:25 +0800

    Update dy-common.js

--- js/dy-common.js
+++ js/dy-common.js
@@ -1,82 +1,186 @@
-$(document).ready(function() {
-	var temp1=true;
-    
-	//手机版快捷菜单控制
-    var head_menu=$("#dy_menu");
-    var head_nav =$("#head_nav");
-    head_menu.on("click",function(){
-        $(this).children().toggleClass("menu-expand");
-        head_nav.toggleClass("menu-show");
-    });
-    $("#head_nav").on("click",function(){
-    	console.log(11)
-        head_menu.children().removeClass("menu-expand");
-        $("#head_nav").removeClass("menu-show");
-    });
-	
-	//手机版中文切换按钮
-	$('.eng').click(function(e) {
-		if(temp1==true){
-			$(".language-en").css({"display":"none",});
-			$(".language-cn").css({"display":"block",});
-			temp1=false;
-		}else if(temp1==false){
-			$(".language-en").css({"display":"block",});
-			$(".language-cn").css({"display":"none",});
-			temp1=true;
-		}
-	});	
-	//手机版产品列表菜单控制	
-	$(".list-box").click(function(){
-		$(this).toggleClass("menu-box")
-		$(this).parent().siblings(".mobile-menu-list").toggleClass("active");
+//曲线图数据方法
+function pageviewsFunc(xAxis,series) {
+	var dom = document.getElementById("pageviews");
+	var myChart = echarts.init(dom);
+	var app = {};
+	option = null;
+	option = {
+	    title: {
+	        text: '曲线图数据展示',
+			x:'center',
+	        y:'top',
+	        top:'15px',
+	        textAlign:'left'
+	    },
+	    tooltip: {
+	        trigger: 'axis',
+	        axisPointer: {
+	            type: 'cross',
+	            label: {
+	                backgroundColor: '#f3f6fe'
+	            }
+	        }
+	    },
+	    grid: {
+	        left: '3%',
+	        right: '4%',
+	        bottom: '3%',
+	        containLabel: true
+	    },
+	    xAxis: [
+	        {
+	            type: 'category',
+	            boundaryGap: false,
+	            data:xAxis,
+	        }
+	    ],
+	    yAxis: [
+	        {
+	            type: 'value',
+	            axisLabel: {
+		            formatter: '{value} 人'
+		        },
+	        }
+	    ],
+	    series: [
+	        {
+	            name: '邮件营销',
+	            type: 'line',
+	            stack: '总量',
+				areaStyle: {
+					normal: {
+						color: '#f3f6fe' //改变区域颜色
+					}
+				},
+				itemStyle : {
+					normal : {
+						color:'#4586ef', //改变折线点的颜色
+							lineStyle:{
+							color:'#4586ef' //改变折线颜色
+						}
+					}
+				},
+	            data:series,
+	        },
+	    ]
+	};
+	if (option && typeof option === "object") {
+	    myChart.setOption(option, true);
+	}
+}
+//饼图数据方法
+function pieLeftFunc(data){
+	var pieLeft = document.getElementById("pie_left");
+	var myPieLeft = echarts.init(pieLeft);
+	optionPie = {
+	    title: {
+	        text: '饼图数据展示',
+	        left: 'center',
+	        top:'35px'
+	    },
+	    tooltip: {
+	        trigger: 'item',
+	        formatter: '{a} <br/>{b} : {c} ({d}%)'
+	    },
+	    series: [
+	        {
+	            name: '访问来源',
+	            type: 'pie',
+	            radius: '55%',
+	            center: ['50%', '60%'],
+	            data: data,
+	            emphasis: {
+	                itemStyle: {
+	                    shadowBlur: 10,
+	                    shadowOffsetX: 0,
+	                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+	                }
+	            }
+	        }
+	    ]
+	};
+	if (optionPie && typeof optionPie === "object") {
+	    myPieLeft.setOption(optionPie, true);
+	}
+}
+
+function pieRightFunc(xAxis,series) {
+	var pieRight = document.getElementById("pie_right");
+	var myPieRight = echarts.init(pieRight);
+	optionPieRight = {
+	    color: ['#3398DB'],
+		title: {
+	        text: '柱状图数据展示',
+	        left: 'center',
+	        top:'15px'
+	    },
+	    tooltip: {
+	        trigger: 'axis',
+	        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+	            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+	        }
+	    },
+	    grid: {
+	        left: '3%',
+	        right: '4%',
+	        bottom: '3%',
+	        containLabel: true
+	    },
+	    xAxis: [
+	        {
+	            type: 'category',
+	            data: xAxis,
+	            axisTick: {
+	                alignWithLabel: true
+	            }
+	        }
+	    ],
+	    yAxis: [
+	        {
+	            type: 'value'
+	        }
+	    ],
+	    series: [
+	        {
+	            name: '直接访问',
+	            type: 'bar',
+	            barWidth: '60%',
+	            data: series
+	        }
+	    ]
+	};
+	if (optionPieRight && typeof optionPieRight === "object") {
+	    myPieRight.setOption(optionPieRight, true);
+	}
+}
+function monthData(url){
+	var url = url
+	$.ajax({
+	   type: "get",
+	   url: url,
+	   	success: function(res){
+	   		if(res.code ==200) {
+	   			if(url== 'https://edu.telking.com/api/?type=month'){
+	   				pageviewsFunc (res.data.xAxis,res.data.series);
+	   			} else {
+	   				var data = [];
+	   				for(var i in res.data.series) {
+	   					data.push({value:res.data.series[i],name:res.data.xAxis[i]})
+	   				}
+	   				pieLeftFunc(data);
+	   				pieRightFunc(res.data.xAxis,res.data.series)
+	   			}
+	   			
+	   		} else {
+	   			console.log(res);
+	   		}
+	   	},
+		error: function (err) {    
+	        console.log(err);
+	    }   
 	});
-	
-	$(".dy-main .main-conter .mobile-menu-list ul li").click(function(){
- 		$(this).addClass('active');
- 		$(this).siblings().removeClass('active');
- 		$(this).children("div").addClass('active');
- 		$(this).siblings().children("div").removeClass('active');
-	})
-	$(".wx-list").click(function(){
- 		$(this).children(".items-cate").addClass('active');
- 		$(this).siblings().children(".items-cate").removeClass('active');
- 		$(this).parent().parent().siblings().children("div").children().children(".items-cate").removeClass("active");
-	})
-})
-function imgShow(dy_enlarge, innerdiv, bigimg, pimg){  
-	var src = pimg.attr("src");//获取当前点击的pimg元素中的src属性  
-	$(bigimg).attr("src", src);//设置#bigimg元素的src属性 
-    lookup(src) 	//调用lookup函数	      
-	$(dy_enlarge).click(function(){//再次点击淡出消失弹出层  
-		$(this).fadeOut("fast");
-	});  
 }
-function lookup(t){
-    var img = new Image(); //创建图片对象
-    img.src=t
-    var windowW = $(window).width();//获取当前窗口宽度  
-	var windowH = $(window).height();//获取当前窗口高度  
-	var realWidth = img.width;//获取图片真实宽度  
-	var realHeight = img.height;//获取图片真实高度
-	var imgWidth, imgHeight;  
-	var scale = 0.8;//缩放尺寸，当图片真实宽度和高度大于窗口宽度和高度时进行缩放
-	if(realHeight>windowH*scale) {//判断图片高度  
-		imgHeight = windowH*scale;//如大于窗口高度，图片高度进行缩放  
-		imgWidth = imgHeight/realHeight*realWidth;//等比例缩放宽度  
-		if(imgWidth>windowW*scale) {//如宽度扔大于窗口宽度  
-		    imgWidth = windowW*scale;//再对宽度进行缩放  
-		    }  
-	} else if(realWidth>windowW*scale) {//如图片高度合适，判断图片宽度  
-		imgWidth = windowW*scale;//如大于窗口宽度，图片宽度进行缩放  
-		imgHeight = imgWidth/realWidth*realHeight;//等比例缩放高度  
-	} else {//如果图片真实高度和宽度都符合要求，高宽不变  
-		imgWidth = realWidth;  
-		imgHeight = realHeight;  
-	}  
-		$(bigimg).css("width",imgWidth);//以最终的宽度对图片缩放  	          
-		var w = (windowW-imgWidth)/2;//计算图片与窗口左边距  
-		var h = (windowH-imgHeight)/2;//计算图片与窗口上边距  
-		$(innerdiv).css({"top":h, "left":w});//设置#innerdiv的top和left属性  
-		$(dy_enlarge).fadeIn("fast");//淡入显示#outerdiv及.pimg
-}
+var urlMonth = 'https://edu.telking.com/api/?type=month';
+var urlWeek = 'https://edu.telking.com/api/?type=week';
+monthData(urlMonth);
+monthData(urlWeek);
